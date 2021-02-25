226d003cdd12bfb530dde90600004f1c7eb1897e
lukevangorder <lukevangorder@gmail.com>
2021-02-25 11:05:51 -0500

    commit tree 11d2329898ac805b8d9467c545af4fba1e5a6ee2

--- app.rb
+++ app.rb
@@ -5,5 +5,20 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      erb :index
+    end
+
+    get '/new' do
+      erb :new
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate][:name], params[:pirate][:height], params[:pirate][:weight])
+      @ship1 = Ship.new(params[:pirate][:ships][0][:name], params[:pirate][:ships][0][:type], params[:pirate][:ships][0][:booty])
+      @ship2 = Ship.new(params[:pirate][:ships][1][:name], params[:pirate][:ships][1][:type], params[:pirate][:ships][1][:booty])
+      erb :show
+    end
+
   end
 end
